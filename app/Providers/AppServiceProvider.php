0cfd9a394d455b1941a734a1d51c397cae959bba
nuwudy <info@wensdoc.com>
2026-08-11 21:34:08 +0530

    fix: Tell Curator Glide to use Imagick driver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        \Awcodes\Curator\Facades\Glide::serverConfig([
+            'driver' => 'imagick',
+        ]);
     }
 
     /**
